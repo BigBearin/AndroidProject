68cab58a98b3a9458727ef0640e98a5114a25b75
BigBearin <brandinbarren@hotmail.com>
2015-02-16 17:49:43 -0600

    Updated code parameters

--- DayThree/app/src/main/java/com/daythree/daythree/MainActivity.java
+++ DayThree/app/src/main/java/com/daythree/daythree/MainActivity.java
@@ -18,17 +18,16 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        etInput = (EditText)findViewById(R.id.et_input);
+        etInput = (EditText) findViewById(R.id.et_input);
         tvReact = (TextView) findViewById(R.id.tv_react);
-
 
 
         tvReact.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this,"What is a toast?", Toast.LENGTH_LONG).show();
+                Toast.makeText(MainActivity.this, "What is a toast?", Toast.LENGTH_LONG).show();
                 Intent intent = new Intent(MainActivity.this, secondActivity.class);
-                intent.putExtra("key",etInput.getText().toString());
+                intent.putExtra("key", etInput.getText().toString());
                 startActivity(intent);
 
             }
